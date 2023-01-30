79f9c2e285ed778bc1bbf26a6d72a2c34d24c5db
dmitriy <dima-dim@yandex.ru>
2023-01-30 10:53:13 +0300

    Задача 54 решена

--- Zadacha54/Program.cs
+++ Zadacha54/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+void PrintArray(int[,] inArray) //Отображение массива
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] GetArray(int m, int n, int minValue, int maxValue) //Создание массива
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return result;
+}
+
+int[,] ArrayStringReduction(int[,] array) //Элементы в строке по порядку
+{
+
+    for (int row = 0; row < array.GetLength(0); row++)
+    {
+        for (int coll = 0; coll < array.GetLength(1); coll++)
+        {
+            for (int k = coll + 1; k < array.GetLength(1); k++)
+            {
+                if (array[row, k] > array[row, coll])
+                {
+                    int n = array[row, coll];
+                    array[row, coll] = array[row, k];
+                    array[row, k] = n;
+                }
+            }
+        }
+    }
+    return array;
+}
+
+int[,] array = GetArray(5, 4, 1, 9);
+PrintArray(array); 
+
+Console.WriteLine("\nОт наибольшего к наименьшему в строке");
+ArrayStringReduction(array);
+PrintArray(array);
